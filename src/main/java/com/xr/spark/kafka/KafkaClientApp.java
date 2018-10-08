40ed3092f532144650d82f1f860ab89934f3554b
xr <xiaorui2017@gmail.com>
2018-10-08 16:48:22 +0800

    kafka生产者消费者

---
+++ src/main/java/com/xr/spark/kafka/KafkaClientApp.java
@@ -0,0 +1,8 @@
+package com.xr.spark.kafka;
+
+public class KafkaClientApp {
+    public static void main(String[] args) {
+        new KafkaProducer(KafkaProperties.TOPIC).start();
+        new KafkaConsumer(KafkaProperties.TOPIC).start();
+    }
+}
---
+++ src/main/java/com/xr/spark/kafka/KafkaProperties.java
@@ -0,0 +1,10 @@
+package com.xr.spark.kafka;
+/**
+ * kafka常用配置文件
+ * */
+public class KafkaProperties {
+    public static final String  ZK= "192.168.0.132";
+    public static final String TOPIC="hello_topic";
+    public static final String BROKER_LIST="192.168.0.132:9092";
+    public static final String GROUP_ID="test_group1";
+}
